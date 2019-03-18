049f336f909d60ef751e98f61e133c02a032cd33
Veenits123 <123sveenit@gmail.com>
2019-03-18 10:47:25 +0530

    third commit

--- js/script.js
+++ js/script.js
@@ -36,5 +36,19 @@ $(document).ready(function() {
     });
     $('.gallery-list-item').click(function() {
         $(this).addClass('active-item').siblings().removeClass('active-item');
-    })
+    });
+    $(window).scroll(function() {
+        let position = $(this).scrollTop();
+        if (position >= 4300) {
+            $('.card-1').addClass("moveFromLeft");
+            $('.card-2').addClass("moveFromBottom");
+            $('.card-3').addClass("moveFromRight");
+
+        } else {
+            $('.card-1').removeClass("moveFromLeft");
+            $('.card-2').removeClass("moveFromBottom");
+            $('.card-3').removeClass("moveFromRight");
+
+        }
+    });
 });
